367c901988ae0162035f4a334939eb0fcc5c5b94
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2022-12-19 23:12:48 +0600

    sign in and sign up basic implemented

--- app/src/main/java/com/example/promap/view/MainActivity.java
+++ app/src/main/java/com/example/promap/view/MainActivity.java
@@ -11,19 +11,32 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.example.promap.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity{
     public static Menu menu;
     Fragment selectedFragment;
     Navigator navigator = new Navigator();
+    Button btnLogOut;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
         getSupportFragmentManager().beginTransaction().replace(R.id.id_fragment_controller,
                 new HomeFragment()).commit();
@@ -44,5 +57,13 @@ public class MainActivity extends AppCompatActivity{
                     selectedFragment).commit();
             return true;
         });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 }
